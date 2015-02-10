3573538e1ea8e3c54a21506c8aea50c9536e1e87
gengyixiong <gengyixiong@gmail.com>
2015-02-10 09:57:19 -0500

    screenshot

--- app/src/main/java/gengyixiong/me/camerademo/MainActivity.java
+++ app/src/main/java/gengyixiong/me/camerademo/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -32,17 +33,16 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.imageView);
         button    = (Button) findViewById(R.id.buttton);
-
-        file      = new File(this.getExternalFilesDir(null),"image.jpg");
-        uri       = Uri.fromFile(file);
+        file      = new File(this.getExternalFilesDir(null),"image.jpg");       //file cannot be seen by media gallery using this path
+        uri       = Uri.fromFile(file);                                         //create uri from a file
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);    //intent sent to camera capture
+                intent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);        //orientation of picture
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-                startActivityForResult(intent, 2);
+                startActivityForResult(intent, 2);                              //return to onActivityResult
             }
         });
     }
@@ -53,23 +53,22 @@ public class MainActivity extends ActionBarActivity {
         if(resultCode == RESULT_OK){
             if(requestCode == 2){
                 startPhotoZoom(uri);
-            }else{
+            }else{                      //requestcode == 3 in this case
                 try{
                     BitmapFactory.Options options = new BitmapFactory.Options();
                     options.inSampleSize = 2;
                     Bitmap bitmap = BitmapFactory.decodeFile(file.getPath(), options);
-                    bitmap = compressImage(bitmap, 50000);
+                    bitmap = compressImage(bitmap, 1000);
 
                     if (bitmap != null){
                         imageView.setImageBitmap(bitmap);
-                        FileOutputStream fos = null;
-                        fos = new FileOutputStream(file);
+                        FileOutputStream fos = new FileOutputStream(file);
                         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
                         fos.flush();
                         fos.close();
                     }
                 }catch (Exception e){
-
+                    Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
                 }
             }
         }
@@ -81,8 +80,8 @@ public class MainActivity extends ActionBarActivity {
         intent.putExtra("crop", "true");
         intent.putExtra("aspectX", 1);
         intent.putExtra("aspectY", 1);
-        intent.putExtra("outputX", 200);
-        intent.putExtra("outputY", 200);
+        intent.putExtra("outputX", 500);        //image size
+        intent.putExtra("outputY", 500);
         intent.putExtra("output", uri);
         intent.putExtra("outputFormat","JPEG");
         startActivityForResult(intent, 3);
@@ -99,8 +98,7 @@ public class MainActivity extends ActionBarActivity {
             image.compress(Bitmap.CompressFormat.JPEG, options, baos);
         }
         ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-        Bitmap bitmap = BitmapFactory.decodeStream(bais, null, null);
-        return bitmap;
+        return BitmapFactory.decodeStream(bais, null, null);
     }
 
     @Override
